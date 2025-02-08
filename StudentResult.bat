f6ce22878283ec1c942a67663ea86afd59e42a25
caleboppong <maroppcal@hotmail.it>
2025-02-08 22:20:41 +0000

    Add files via upload

---
+++ StudentResult.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Navigate to the script's directory
+cd /d "C:\Users\marop\Documents\Elizabeth School of London\Semester 2 Assessments"
+
+REM Run the Python script
+python Process_Data_grades_average_passfail.py
+
+REM Pause to see output
+pause
